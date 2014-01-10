3a6a671c92a287778a571442e4713704a6373aa6
Ryan Weaver <ryan@thatsquality.com>
2014-01-10 17:22:46 -0600

    Getting the access token via client credentials

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -10,6 +10,18 @@ $http = new Client('http://coop.apps.knpuniversity.com', array(
     )
 ));
 
+/* 1. Get the Access Token */
+$request = $http->post('/token', null, array(
+    'client_id'     => 'Brent\'s Lazy CRON Job',
+    'client_secret' => 'a2e7f02def711095f83f2fb04ecbc0d3',
+    'grant_type'    => 'client_credentials',
+));
+
+// make a request to the token url
+$response = $request->send();
+$responseBody = $response->getBody(true);
+var_dump($responseBody);die;
+
 $accessToken = 'GET THIS FROM YOUR APPLICATION FOR NOW';
 
 $request = $http->post('/api/2/eggs-collect');
