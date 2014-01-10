c643979eff8767be6540fc2777c1a0a9fe773e86
Ryan Weaver <ryan@thatsquality.com>
2014-01-10 16:16:01 -0600

    Changing Client to be simply http
    I'm going to try to avoid the user of "client" in the HTTP sense, so that we don't confuse
    it with an OAuth client :/

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -4,7 +4,7 @@ include __DIR__.'/vendor/autoload.php';
 use Guzzle\Http\Client;
 
 // create our http client (Guzzle)
-$client = new Client('http://coop.apps.knpuniversity.com', array(
+$http = new Client('http://coop.apps.knpuniversity.com', array(
     'request.options' => array(
         'exceptions' => false,
     )
