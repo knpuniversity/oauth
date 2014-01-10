f491e8d474e8253772040e5eb134694b4e82a100
Ryan Weaver <ryan@thatsquality.com>
2014-01-10 15:35:07 -0600

    Going backwards a little bit more for the collect_eggs.php script

--- cron/collect_eggs.php
+++ cron/collect_eggs.php
@@ -6,9 +6,4 @@ use Guzzle\Http\Client;
 // create our http client (Guzzle)
 $client = new Client('http://coop.apps.knpuniversity.com');
 
-/* 1. Get the Access Token */
-$request = $client->post('/token', null, array(
-    'client_id'     => '',
-    'client_secret' => '',
-    'grant_type'    => '',
-));
+
