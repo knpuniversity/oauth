720256e505e7b78766337340bcda29530f7746c8
Ryan Weaver <ryan@thatsquality.com>
2014-02-02 12:45:56 -0600

    Adding application edit support for server

--- server/src/OAuth2Demo/Server/Controllers/AppManagement.php
+++ server/src/OAuth2Demo/Server/Controllers/AppManagement.php
@@ -3,6 +3,9 @@
 namespace OAuth2Demo\Server\Controllers;
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 
 class AppManagement
 {
@@ -12,6 +15,8 @@ class AppManagement
         $routing->get('/application', [new self(), 'create'])->bind('app_create');
         $routing->post('/application', [new self(), 'add'])->bind('app_add');
         $routing->get('/application/{name}', [new self(), 'show'])->bind('app_show');
+        $routing->get('/application/edit/{name}', [new self(), 'edit'])->bind('app_edit');
+        $routing->post('/application/edit/{name}', [new self(), 'update'])->bind('app_update');
     }
 
     /**
@@ -31,20 +36,11 @@ class AppManagement
             return $app['twig']->render('app\create.twig', ['error' => '"name" is required']);
         }
 
-        // get the requested client scope
-        $scope = implode(' ', $app['request']->request->get('scope', []));
-
-        // get the requested redirect_uri
-        $redirect_uri = $app['request']->request->get('redirect_uri');
-
-        // generate a random secret
-        $secret = substr(md5(microtime()), 0, 32);
-
-        // get the logged-in user and tie it to the newly-created client
-        $user = $app['security']->getToken()->getUser();
-
-        // create the client
-        $app['storage']->setClientDetails($name, $secret, $redirect_uri, null, $scope, $user->getUsername());
+        $this->saveClientDetailsFromRequest(
+            $name,
+            $app['request'],
+            $app
+        );
 
         return $app['twig']->render('app\show.twig', [
             'client' => $app['storage']->getClientDetails($name),
@@ -64,5 +60,55 @@ class AppManagement
         return $app['twig']->render('app\show.twig', [
             'client' => $client,
         ]);
+    }
+
+    public function edit(Application $app, $name)
+    {
+        if (!$client = $app['storage']->getClientDetails(urldecode($name))) {
+            $app->abort(404, "Application \"$name\" does not exist.");
+        }
+
+        $client['scope_arr'] = explode(' ', $client['scope']);
+
+        return $app['twig']->render('app\edit.twig', array(
+            'client' => $client,
+            'editName' => false,
+        ));
+    }
+
+    public function update(Application $app, $name)
+    {
+        if (!$client = $app['storage']->getClientDetails(urldecode($name))) {
+            $app->abort(404, "Application \"$name\" does not exist.");
+        }
+
+        $this->saveClientDetailsFromRequest($name, $app['request'], $app);
+
+        $url = $app['url_generator']->generate('app_show', array('name' => $name));
+
+        return new RedirectResponse($url);
+    }
+
+    private function saveClientDetailsFromRequest($clientId, Request $request, Application $app)
+    {
+        // get the requested client scope
+        $scope = implode(' ', $request->request->get('scope', []));
+
+        // get the requested redirect_uri
+        $redirect_uri = $request->request->get('redirect_uri');
+
+        $clientDetails = $app['storage']->getClientDetails(urldecode($clientId));
+        if ($clientDetails) {
+            $secret = $clientDetails['client_secret'];
+        } else {
+            // generate a random secret
+            $secret = substr(md5(microtime()), 0, 32);
+        }
+
+        // get the logged-in user and tie it to the newly-created client
+        $user = $app['security']->getToken()->getUser();
+
+        // create the client
+        $app['storage']->setClientDetails($clientId, $secret, $redirect_uri, null, $scope, $user->getUsername());
     }
 }
